294ffb3fcc86df8d9e1c352091ed38566158f310
jRodPerez13 <nemesis__13@hotmail.com>
2023-01-28 00:29:35 -0600

    JS in Blazor Add Toastr to Project JSRuntime Extentions Sweet Alerts Referencing Components

---
+++ HiddenVilla.Server/Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+namespace HiddenVilla.Server.Helper;
+
+public static class IJSRuntimeExtensions
+{
+    public static async ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string message)
+    {
+        await jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+    public static async ValueTask ToastrError(this IJSRuntime jSRuntime, string message)
+    {
+        await jSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}
