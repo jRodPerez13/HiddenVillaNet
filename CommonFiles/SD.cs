a4ee9fd05e694fb11166e98241c4acc1bc502ea5
jRodPerez13 <nemesis__13@hotmail.com>
2023-01-30 18:35:18 -0600

    Authorize Attribute Authentication State Adding roles access to pages

---
+++ CommonFiles/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonFiles;
+
+public static class SD //Static Details
+{
+    public const string Role_Admin = "Admin";
+    public const string Role_Customer = "Customer";
+    public const string Role_Employee = "Employee";
+}
