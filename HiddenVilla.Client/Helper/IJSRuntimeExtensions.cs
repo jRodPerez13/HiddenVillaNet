4b23cebccbf90778f01f310dfa4811b41a1c0bb7
jRodPerez13 <nemesis__13@hotmail.com>
2023-01-31 18:47:38 -0600

    Add Toastr, setup css and js cdn

---
+++ HiddenVilla.Client/Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace HiddenVilla.Client.Helper;
+public static class IJSRuntimeExtensions
+{
+    public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+    {
+        await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+    public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+    {
+        await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}
