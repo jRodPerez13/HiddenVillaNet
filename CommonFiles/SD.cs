64b9600f51d23a641a1492b2377c114303d5e31a
jRodPerez13 <nemesis__13@hotmail.com>
2023-02-01 15:11:11 -0600

    Local Storage Loading Gif

--- CommonFiles/SD.cs
+++ CommonFiles/SD.cs
@@ -5,4 +5,6 @@ public static class SD //Static Details
     public const string Role_Admin = "Admin";
     public const string Role_Customer = "Customer";
     public const string Role_Employee = "Employee";
+
+    public const string Local_InitialBooking = "InitialRoomBookingInfo";
 }
