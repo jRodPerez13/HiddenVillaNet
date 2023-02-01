26cf7e8872b3fa6563c4cfbeb1c2f38f083c9c80
jRodPerez13 <nemesis__13@hotmail.com>
2023-02-01 12:25:00 -0600

    Home View Model

---
+++ HiddenVilla.Client/Model/ViewModel/HomeVM.cs
@@ -0,0 +1,7 @@
+﻿namespace HiddenVilla.Client.Model.ViewModel;
+public class HomeVM
+{
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime EndDate { get; set; }
+    public int NoOfNights { get; set; } = 1;
+}
