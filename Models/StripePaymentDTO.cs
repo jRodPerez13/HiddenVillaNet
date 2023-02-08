5ddfbbd6fedf199d2fd1b5a675cd6ad353a1d50e
jRodPerez13 <nemesis__13@hotmail.com>
2023-02-07 18:03:22 -0600

    Stripe Keys Stripe API Endpoint Create Stripe Endpoint in API

--- Models/StripePaymentDTO.cs
+++ Models/StripePaymentDTO.cs
@@ -2,7 +2,7 @@
 public class StripePaymentDTO
 {
     public string? ProductName { get; set; }
-    public double Amount { get; set; }
+    public long? Amount { get; set; }
     public string? ImageUrl { get; set; }
     public string? ReturnUrl { get; set; }
 }
