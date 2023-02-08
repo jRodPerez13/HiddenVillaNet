aea2ff8c998d766a8a275aabfef4b2619eabd53d
jRodPerez13 <nemesis__13@hotmail.com>
2023-02-07 18:21:14 -0600

    Room Order Endpoint in API

---
+++ HiddenVilla.Api/Controllers/RoomOrderController.cs
@@ -0,0 +1,34 @@
+﻿using BussinesLogic.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace HiddenVilla.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class RoomOrderController : Controller
+{
+    private readonly IRoomOrderDetailsRepository _repository;
+
+    public RoomOrderController(IRoomOrderDetailsRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] RoomOrderDetailsDTO details)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _repository.Create(details);
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest(new ErrorModel()
+            {
+                ErrorMessage = "Error while creating Room Details/ Booking"
+            });
+        }
+    }
+}
--- HiddenVilla.Client/Service/RoomOrderDetailsService.cs
+++ HiddenVilla.Client/Service/RoomOrderDetailsService.cs
@@ -1,24 +1,40 @@
 ﻿using HiddenVilla.Client.Service.IService;
 using Models;
+using Newtonsoft.Json;
+using System.Text;
 
-namespace HiddenVilla.Client.Service
+namespace HiddenVilla.Client.Service;
+
+public class RoomOrderDetailsService : IRoomOrderDetailsService
 {
-    public class RoomOrderDetailsService : IRoomOrderDetailsService
+    private readonly HttpClient _client;
+
+    public RoomOrderDetailsService(HttpClient client)
     {
-        private readonly HttpClient _client;
+        _client = client;
+    }
+    public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
+    {
+        throw new NotImplementedException();
+    }
 
-        public RoomOrderDetailsService(HttpClient client)
-        {
-            _client = client;
-        }
-        public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(RoomOrderDetailsDTO details)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
+    {
+        var content = JsonConvert.SerializeObject(details);
+        var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync("api/roomorder/create", bodyContent);
 
-        public Task<RoomOrderDetailsDTO> SaveRoomOrderDetails(RoomOrderDetailsDTO details)
+        if (response.IsSuccessStatusCode)
         {
-            throw new NotImplementedException();
+            var contentTemp = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<RoomOrderDetailsDTO>(contentTemp);
+            return result;
+        }
+        else
+        {
+            var contentTemp = await response.Content.ReadAsStringAsync();
+            var errorModel = JsonConvert.DeserializeObject<ErrorModel>(contentTemp);
+            throw new Exception(errorModel.ErrorMessage);
         }
     }
 }
